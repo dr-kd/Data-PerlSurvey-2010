9b4e57843a043948619cc9b0b7cd24a7e461e77f
Kieren Diment <kd@fenchurch-2.local>
2010-06-06 10:50:39 +0200

    get info about countries

--- script/clean_countries.pl
+++ script/clean_countries.pl
@@ -3,24 +3,41 @@ use warnings;
 use strict;
 use Text::CSV_XS;
 use FindBin qw/$Bin/;
-my $num = 4; # country of birth
-if ($ARGV[0]) { $num = 5 } # country of residence;
 
 my $file = "$Bin/../data/all_data.csv";
+my $outfile = "$Bin/../data/all_data_countries_cleaned.csv";
+open my $OUT, ">", $outfile;
 my $csv = Text::CSV_XS->new ({ binary => 1, eol => $/ });
 open my $io, "<", $file or die "$file: $!";
-while (my $row = $csv->getline ($io)) { 
-        $row->[$num] = lc($row->[$num]);
-        $row->[$num] =~ s/^(united states( of america)?|us|u\.s(\.a)?(\.)?|us of a)$/usa/i;
-        $row->[$num] =~ s/^(russia|ussr|soviet union|ru)$/russian federation/i;
-        $row->[$num] =~ s/^(the netherlands|nl)$/netherlands/i;
-        $row->[$num] =~ s/^(ch|switzerland`|swiss)$/switzerland/i;
-        $row->[$num] =~ s/^(uk$|.*?united kin?gdom.*?|scotland|wales|great britain)$/united kingdom/i;
-        $row->[$num] =~ s/^(de|german democratic republic|gemany)$/germany/i;
-        $row->[$num] =~ s/^no$/norway/i;
-        $row->[$num] =~ s/^france, of course$/france/i;
-        $row->[$num] =~ s/^at$/austria/i;
-        print $row->[$num], "\n";
 
-}
+    while (my $row = $csv->getline ($io)) {
+        for my $num (4,5) {
+            $row->[$num] = lc($row->[$num]);
+            $row->[$num] =~ s/^((the )?unite[ds] states( of america| minor outlying islands)?|us|u\.s(\.a)?(\.)?|us of a)$/usa/i;
+            $row->[$num] =~ s/(^\s+)|(\s+$)//i;
+            $row->[$num] =~ s/^$/not stated/i;
+            $row->[$num] =~ s/^(unknown|confidential)$/not stated/i;
+            $row->[$num] =~ s/^viet nam$/vietnam/i;
+            $row->[$num] =~ s/^(the )?republic of//i;
+            $row->[$num] =~ s/^.*?korea.*$/korea/i;
+            $row->[$num] =~ s/^brasil*$/brazil/i;
+            $row->[$num] =~ s/^catalunya/catalonia/i;
+            $row->[$num] =~ s/^(czechoslovakie|cz)$/czech republic/i;
+            $row->[$num] =~ s/^iran, islamic republic of$/iran/i;
+            $row->[$num] =~ s/^(russ(ia|ai)|ud?ssr|soviet union|ru)$/russian federation/i;
+            $row->[$num] =~ s/^(the netherlands|nl)$/netherlands/i;
+            $row->[$num] =~ s/^(ch|switzerland`|swiss)$/switzerland/i;
+            $row->[$num] =~ s/^(uk$|.*?united kin?gdom.*?|scott?land|wales|england|(great )?britain)$/united kingdom/i;
+            $row->[$num] =~ s/^(de|ger||german democratic republic|gemany)$/germany/i;
+            $row->[$num] =~ s/^no$/norway/i;
+            $row->[$num] =~ s/^au$/australia/i;
+            $row->[$num] =~ s/^france, of course$/france/i;
+            $row->[$num] =~ s/^nzl?$/new zealand/i;
+            $row->[$num] =~ s/^at$/austria/i;
+            $row->[$num] =~ s/^dk$/denmark/i;
+            $row->[$num] =~ s/^jerusalem$/israel/i;
+        }
+        my @newrow = $csv->combine(@$row);
+        print $OUT $csv->string(@newrow), "\n";
+    }
 
