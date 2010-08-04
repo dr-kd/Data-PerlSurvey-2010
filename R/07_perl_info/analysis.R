5e08f45210c1af5b72d0493cf14cd72ab5d59829
Kieren Diment <kd@fenchurch-2.local>
2010-08-05 00:40:47 +1000

    part 7

--- R/07_perl_info/analysis.R
+++ R/07_perl_info/analysis.R
@@ -1,16 +1,16 @@
 load("07_perl_info.RData")
+source("../utils.R")
 data$version_of_perl_used_most_frequently <- ordered(data$version_of_perl_used_most_frequently, levels=c(1:6),labels=c("<=5.4","5.5005","5.6.x","5.8.x","5.10.x","5.12"))
-pdf("freq.pdf")
+version <- category.matrix(data[1:6], "^versions_of_perl_commonly_used_")
+png("freq.png")
 plot( data$version_of_perl_used_most_frequently)
 dev.off()
 
-library(Hmisc)
-describe(sapply(data[1:6],ordered))
+library(R2HTML)
+HTMLStart(".", 'index', HTMLframe=FALSE, Title="Perl Versions", autobrowse=FALSE);
+HTML.title("Perl Versions", HR=1)
+HTML.title("Version of perl most frequently used", HR=2)
+HTMLInsertGraph("freq.png")
 
-# 5.4 (113, 2%) 
-# 5.0005 (142, 3%) 
-# 5.6 (548, 11%) 
-# 5.8 (2756, 57%) 
-# 5.10 (2728, 56%) 
-# 5.12 (628, 13%) 
-
+HTML.title("Versions of perl commonly used ", HR=2)
+HTML(version,row.names=TRUE)
