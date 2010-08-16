9efa7ccffc127d60d3c28b940adc2dd166d62fdd
Kieren Diment <kd@fenchurch-2.local>
2010-08-16 14:10:20 +1000

    CPAN modules

--- R/14_cpan_info/analysis.R
+++ R/14_cpan_info/analysis.R
@@ -1,19 +1,23 @@
 load("14_cpan_info.RData")
-data$num_cpan_modules <- ordered(data$num_cpan_modules, levels=c(1:4), labels=c("None","1 - 5","6 - 20","21+"))
-data$num_cpan_modules_handed_over <- ordered(data$num_cpan_modules_handed_over, levels=c(1:4), labels=c("None","1 - 5","6 - 20","21+"))
+data$num_cpan_modules <- ordered(data$num_cpan_modules, levels=c(1:4), labels=c("None","1-5","6-20","21+"))
+data$num_cpan_modules_handed_over <- ordered(data$num_cpan_modules_handed_over, levels=c(1:4), labels=c("None","1-5","6-20","21+"))
 
-pdf("cpan.pdf")
-par(mfrow=c(1,2),las=2)
-plot(data[1],main="Number of CPAN modules")
-plot(data[2],main="Modules handover")
+png("modules.png")
+par(las=2)
+plot(data[1],main="Number of CPAN Modules")
+dev.off()
+png("handover.png")
+par(las=2)
+plot(data[2],main="Number of CPAN Modules Handed Over")
 dev.off()
 
 
-# number of cpan modules
-  None  1 - 5 6 - 20    21+ 
- 78.60  15.85   4.02   2.11 
+library(R2HTML)
+HTMLStart(".", 'index', HTMLframe=FALSE, Title="Cpan Modules Maintained", autobrowse=FALSE);
+HTML.title("CPAN modules maintained / handed over to others", HR=1)
+HTMLInsertGraph("modules.png")
+HTMLInsertGraph("handover.png")
+HTMLStop()
 
-# number of cpan modules handed over
 
-  None  1 - 5 6 - 20    21+ 
- 91.35   6.95   1.04   0.66 
+
