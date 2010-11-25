7f4694a9024b3e0781cad154d352d9360b18f48c
Kieren Diment <kd@bld39-ach-172-30-17-195.wlan.uow.edu.au>
2010-11-25 13:35:50 +1100

    final report :)

--- R/21_perl_cert/analysis.R
+++ R/21_perl_cert/analysis.R
@@ -1,6 +1,6 @@
 load("21_perl_cert.RData")
 write.csv(data,file="21_perl_cert.csv")
-pdf("cert.pdf")
+png("cert.png")
 par(mfrow=c(2,1))
 plot(ordered(data$official_perl_cert_for_me),main="for me")
 plot(ordered(data$official_perl_cert_for_others),main="for others")
